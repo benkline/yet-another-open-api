c7a8bb3c5ef1ac221201521d4c13c5a026e53ddd
Ben Kline <ben.kline@gmail.com>
2016-05-20 18:58:17 -0600

    authenticated users api

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
 
+  it "authenticated?" do
+        # authenticate_or_request_with_http_basic {|email, password| User.where( email: email, password: password).present? }
+  end
+
 end
