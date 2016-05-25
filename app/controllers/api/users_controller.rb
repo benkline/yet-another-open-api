e3c3a823a75046db2db74fd77f8e4774a84ac9d0
Ben Kline <ben.kline@gmail.com>
2016-05-25 09:47:07 -0600

    add user, item and list create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,13 +6,20 @@ class Api::UsersController < ApiController
     render json: users, each_serializer: UserSerializer
   end
 
-# api_users     GET    /api/users(.:format)          api/users#index {:format=>:json}
-#               POST   /api/users(.:format)          api/users#create {:format=>:json}
-# new_api_user  GET    /api/users/new(.:format)      api/users#new {:format=>:json}
-# edit_api_user GET    /api/users/:id/edit(.:format) api/users#edit {:format=>:json}
-# api_user      GET    /api/users/:id(.:format)      api/users#show {:format=>:json}
-#               PATCH  /api/users/:id(.:format)      api/users#update {:format=>:json}
-#               PUT    /api/users/:id(.:format)      api/users#update {:format=>:json}
-#               DELETE /api/users/:id(.:format)      api/users#destroy {:format=>:json}
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :bio, :name)
+  end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :name, :email, :password, :bio
 
-   def name
-     object.name
-   end
+  #  def name
+  #    object.name
+  #  end
 
    def created_at
      object.created_at.strftime('%B %d, %Y')
